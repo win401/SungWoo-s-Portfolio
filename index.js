0e5f865778b7b4f8015de13e3d06a90a849ea959
win401 <winnerv401@gmail.com>
2023-06-28 16:08:30 +0900

    오픈그래프

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const bg1 = document.querySelector(".radius_background");
+  const gradient = document.querySelector(".shadow_gradient");
+  const star = document.querySelector("#star6");
+
+  this.addEventListener("mousemove", (evt) => {
+    let x = evt.clientX;
+    let y = evt.clientY;
+
+    bg1.style.transform = `translate(${-x / 60}px,${y / 60}px)`;
+    gradient.style.transform = `translate(${-x / 60}px,${-y / 60}px)`;
+    star.style.transform = `translate(${-x / 60}px,${-y / 60}px)`;
+  });
+});
