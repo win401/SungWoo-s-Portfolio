0af2fc5d2a8c34d8a15c38bb65345f154ab87a15
win401 <winnerv401@gmail.com>
2023-06-29 17:56:57 +0900

    project text 수정

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const bg1 = document.querySelector(".radius_background");
-  const gradient = document.querySelector(".shadow_gradient");
-  const star = document.querySelector("#star6");
+const bg1 = document.querySelector(".radius_background");
+const gradient = document.querySelector(".shadow_gradient");
+const star = document.querySelector("#star6");
 
-  this.addEventListener("mousemove", (evt) => {
-    let x = evt.clientX;
-    let y = evt.clientY;
+this.addEventListener("mousemove", (evt) => {
+  let x = evt.clientX;
+  let y = evt.clientY;
 
-    bg1.style.transform = `translate(${-x / 15}px,${0}px)`;
-    gradient.style.transform = `translate(${-x / 15}px,${0}px)`;
-    star.style.transform = `translate(${-x / 60}px,${-y / 60}px)`;
-  });
+  bg1.style.transform = `translate(${-x / 15}px,${-y / 100}px)`;
+  gradient.style.transform = `translate(${-x / 15}px,${0}px)`;
+  star.style.transform = `translate(${-x / 60}px,${-y / 60}px)`;
+});
+
+// project1 헬리녹스 이미지
+$(".pj1").on("mouseenter", () => {
+  $(".project1").addClass("active");
+});
+$(".pj1").on("mouseleave", () => {
+  $(".project1").removeClass("active");
+});
+
+// project2 한림공원 이미지
+$(".pj2").on("mouseenter", () => {
+  $(".project2").addClass("active");
+});
+$(".pj2").on("mouseleave", () => {
+  $(".project2").removeClass("active");
 });
