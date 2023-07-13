cf71326a1221f977dbd03fb96b75c6048743aa77
win401 <winnerv401@gmail.com>
2023-07-13 15:46:20 +0900

    수정

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ this.addEventListener('mousemove', (evt) => {
   star.style.transform = `translate(${-x / 60}px,${-y / 60}px)`;
 });
 
+// 현재시간 출력
+// const date = document.querySelector('.date span');
+const hours = document.querySelector('.hours span');
+const minutes = document.querySelector('.minutes span');
+const seconds = document.querySelector('.seconds span');
+const now = new Date();
+
+// 재귀함수 방법
+function updateTime() {
+  const now = new Date();
+  // date.textContent = String(now.getDate()).padStart(2, '0');
+  hours.textContent = String(now.getHours()).padStart(2, '0');
+  minutes.textContent = String(now.getMinutes()).padStart(2, '0');
+  seconds.textContent = String(now.getSeconds()).padStart(2, '0');
+
+  setTimeout(updateTime, 1000); // setInterval()을 이용하는 것보다 메모리 측면에서 효율적
+}
+updateTime();
+
 // project1 헬리녹스 이미지
 $('.pj1').on('mouseenter', () => {
   $('.project1').addClass('active');
